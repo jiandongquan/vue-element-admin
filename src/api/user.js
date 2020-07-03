5438d50f31edc8fa71991044bb97650764fc93f5
paul <jiandongquan@126.com>
2020-07-03 16:55:01 +0800

    增加注释

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,6 @@
 import request from '@/utils/request'
 
+// 用户登陆
 export function login(data) {
   return request({
     url: '/vue-element-admin/user/login',
@@ -8,6 +9,7 @@ export function login(data) {
   })
 }
 
+// 获取用户信息
 export function getInfo(token) {
   return request({
     url: '/vue-element-admin/user/info',
@@ -16,6 +18,7 @@ export function getInfo(token) {
   })
 }
 
+// 登出系统
 export function logout() {
   return request({
     url: '/vue-element-admin/user/logout',
